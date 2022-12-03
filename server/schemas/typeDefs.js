4ab17002424802070a7c7bf0a76886cfcfe20235
Justin Liu <justinwl2097@gmail.com>
2022-12-03 12:49:33 -0600

    implemented typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,48 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type Query {
+        me: User
+    }
 
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(saveBookParams): User
+        removeBook(bookId: String!): User
+    }
+
+    input saveBookParams {
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
+        authors: [String]
+    }
 `;
 
 module.exports = typeDefs;
